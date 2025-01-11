af2fb6af1366b6318272940d5c6bb3de2ebb6299
sirix777 <sirix@inbox.lv>
2025-01-11 16:03:46 +0200

    cs: Update PHP-CS-Fixer config with rule changes and path fixes
    Renamed the directory from "tests" to "test" for consistency and added the "phpdoc_to_comment" rule set to false. This ensures better alignment with project conventions and improves code style enforcement.

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -6,7 +6,10 @@ use Sirix\CsFixerConfig\ConfigBuilder;
 
 return ConfigBuilder::create()
     ->inDir(__DIR__.'/src')
-    ->inDir(__DIR__ . '/tests')
+    ->inDir(__DIR__ . '/test')
 	->inDir(__DIR__ . '/config')
+    ->setRules([
+        'phpdoc_to_comment' => false
+    ])
     ->getConfig()
 ;
